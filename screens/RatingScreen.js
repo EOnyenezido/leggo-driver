267975a40b923c6f3a2807d2046cf70667ced5ff
sleonvictor <v.onyenezido@gmail.com>
2019-12-07 00:59:09 +0100

    Add rating screen

--- screens/RatingScreen.js
+++ screens/RatingScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image, StyleSheet, ProgressBarAndroid } from 'react-native';
 import { Container, Content, Text, Item, Input, Button, StyleProvider } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
@@ -9,7 +9,35 @@ import colors from '../constants/Colors';
 import getTheme from '../native-base-theme/components';
 import platform from '../native-base-theme/variables/platform';
 
+import firebase from '../services/firebase';
+
 export default function RatingScreen() {
+  const [reviews, setReviews] = useState([]);
+  const [rating, setRating] = useState(0);
+  const [stars, setStars] = useState({5: 0, 4: 0, 3: 0, 2: 0, 1: 0});
+  const db = firebase.firestore();
+  useEffect(() => {
+    const unsubscribe = db.collection("reviews").where("driver", "==", "1816")
+    .onSnapshot(function(querySnapshot) { 
+      let newOrders = [];       
+      querySnapshot.forEach(function(doc) {
+        newOrders.push({id: doc.id, ...doc.data()});
+      });
+      setReviews(newOrders);
+      const normalizedStars = {5: 0, 4: 0, 3: 0, 2: 0, 1: 0};
+      const total = newOrders.reduce((total, item) => {
+        normalizedStars[item.stars]+=1;
+        return total + item.stars;
+      }, 0);
+      setRating((total/newOrders.length).toFixed(2));
+      setStars(normalizedStars);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [rating.length]);
+
   return (
     <StyleProvider style={getTheme(platform)}>
       <Container>
@@ -27,23 +55,28 @@ export default function RatingScreen() {
             </Row>
             <Row>
               <Col style={styles.mainCol}>
-                <Text style={styles.mainText}>3.26</Text>
+                <Text style={styles.mainText}>{rating}</Text>
               </Col>
             </Row>
             <Row style={styles.ratingRow}>
-              <Image source={ require('../assets/images/rating.png') }
+              <Image source={ Math.floor(rating) >= 1 ? require('../assets/images/rating.png')
+                : require('../assets/images/rating_blank.png') }
                   style={styles.ratingImage}
               />
-              <Image source={ require('../assets/images/rating.png') }
+              <Image source={ Math.floor(rating) >= 2 ? require('../assets/images/rating.png')
+                : require('../assets/images/rating_blank.png') }
                   style={styles.ratingImage}
               />
-              <Image source={ require('../assets/images/rating.png') }
+              <Image source={ Math.floor(rating) >= 3 ? require('../assets/images/rating.png')
+                : require('../assets/images/rating_blank.png') }
                   style={styles.ratingImage}
               />
-              <Image source={ require('../assets/images/rating_blank.png') }
+              <Image source={ Math.floor(rating) >= 4 ? require('../assets/images/rating.png')
+                : require('../assets/images/rating_blank.png') }
                   style={styles.ratingImage}
               />
-              <Image source={ require('../assets/images/rating_blank.png') }
+              <Image source={ Math.floor(rating) >= 5 ? require('../assets/images/rating.png')
+                : require('../assets/images/rating_blank.png') }
                   style={styles.ratingImage}
               />
             </Row>
@@ -58,13 +91,30 @@ export default function RatingScreen() {
                 <ProgressBarAndroid
                   styleAttr="Horizontal"
                   indeterminate={false}
-                  progress={0.5}
+                  progress={stars[5] > 0 ? stars[5]/reviews.length : 0}
                   color={colors.yellow}
                   style={styles.progressBar}
                 />
               </Col>
               <Col style={styles.progressCol}>
-                <Text style={styles.progressText}>34 people</Text>
+                <Text style={styles.progressText}>{stars[5]} {stars[5] != 1 ? 'people' : 'person'}</Text>
+              </Col>
+            </Row>
+            <Row>
+              <Col style={styles.progressCol}>
+                <Text style={styles.progressText}>4 star</Text>
+              </Col>
+              <Col style={styles.progressCol}>
+                <ProgressBarAndroid
+                  styleAttr="Horizontal"
+                  indeterminate={false}
+                  progress={stars[4] > 0 ? stars[4]/reviews.length : 0}
+                  color={colors.yellow}
+                  style={styles.progressBar}
+                />
+              </Col>
+              <Col style={styles.progressCol}>
+                <Text style={styles.progressText}>{stars[4]} {stars[4] != 1 ? 'people' : 'person'}</Text>
               </Col>
             </Row>
             <Row>
@@ -75,13 +125,13 @@ export default function RatingScreen() {
                 <ProgressBarAndroid
                   styleAttr="Horizontal"
                   indeterminate={false}
-                  progress={0.2}
+                  progress={stars[3] > 0 ? stars[3]/reviews.length : 0}
                   color={colors.yellow}
                   style={styles.progressBar}
                 />
               </Col>
               <Col style={styles.progressCol}>
-                <Text style={styles.progressText}>34 people</Text>
+                <Text style={styles.progressText}>{stars[3]} {stars[3] != 1 ? 'people' : 'person'}</Text>
               </Col>
             </Row>
             <Row>
@@ -92,51 +142,54 @@ export default function RatingScreen() {
                 <ProgressBarAndroid
                   styleAttr="Horizontal"
                   indeterminate={false}
-                  progress={0.8}
+                  progress={stars[2] > 0 ? stars[2]/reviews.length : 0}
                   color={colors.yellow}
                   style={styles.progressBar}
                 />
               </Col>
               <Col style={styles.progressCol}>
-                <Text style={styles.progressText}>34 people</Text>
+                <Text style={styles.progressText}>{stars[2]} {stars[2] != 1 ? 'people' : 'person'}</Text>
+              </Col>
+            </Row>
+            <Row>
+              <Col style={styles.progressCol}>
+                <Text style={styles.progressText}>1 star</Text>
+              </Col>
+              <Col style={styles.progressCol}>
+                <ProgressBarAndroid
+                  styleAttr="Horizontal"
+                  indeterminate={false}
+                  progress={stars[1] > 0 ? stars[1]/reviews.length : 0}
+                  color={colors.yellow}
+                  style={styles.progressBar}
+                />
+              </Col>
+              <Col style={styles.progressCol}>
+                <Text style={styles.progressText}>{stars[1]} {stars[1] != 1 ? 'people' : 'person'}</Text>
               </Col>
             </Row>
             <Row>
               <Col style={styles.break1} />
             </Row>
-            <Row>
-              <Col style={styles.dotCol}>
-                <Image source={ require('../assets/images/dot.png') }
-                    style={styles.dot}
-                />
-              </Col>
-              <Col style={styles.commentCol}>
-                <Text style={styles.commentText}>Complaint and public affairs on top of you will have a big
-                  impact on getting a higher score than your customers.</Text>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={styles.dotCol}>
-                <Image source={ require('../assets/images/dot.png') }
-                    style={styles.dot}
-                />
-              </Col>
-              <Col style={styles.commentCol}>
-                <Text style={styles.commentText}>Complaint and public affairs on top of you will have a big
-                  impact on getting a higher score than your customers.</Text>
-              </Col>
-            </Row>
-            <Row>
-              <Col style={styles.dotCol}>
-                <Image source={ require('../assets/images/dot.png') }
-                    style={styles.dot}
-                />
-              </Col>
-              <Col style={styles.commentCol}>
-                <Text style={styles.commentText}>Complaint and public affairs on top of you will have a big
-                  impact on getting a higher score than your customers.</Text>
-              </Col>
-            </Row>
+            {
+              reviews.length > 0 ?
+              reviews.slice(0, 3).map((review) => {
+                return(
+                  <Row key={review.id}>
+                    <Col style={styles.dotCol}>
+                      <Image source={ require('../assets/images/dot.png') }
+                          style={styles.dot}
+                      />
+                    </Col>
+                    <Col style={styles.commentCol}>
+                      <Text style={styles.commentText}>{review.comment}</Text>
+                    </Col>
+                  </Row>
+                )
+              })
+              :
+              null
+            }
           </Grid>
         </Content>
       </Container>
@@ -231,12 +284,12 @@ const styles = StyleSheet.create({
     textAlign: 'left',
   },
   commentCol: {
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     alignItems: 'flex-start',
     height: layout.modifier.height(155)
   },
   dotCol: {
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     height: layout.modifier.height(155),
     width: layout.modifier.height(100)
